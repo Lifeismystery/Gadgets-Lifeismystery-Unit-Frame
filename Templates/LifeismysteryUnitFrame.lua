9c89e8c3e41794d90d4859727ed89c9590400a47
Lifeismystery <Lifeismystery@yandex.ru>
2014-01-03 18:51:14 +0400

    1. Unit Frame -  Added options to display combo points.

--- Templates/LifeismysteryUnitFrame.lua
+++ Templates/LifeismysteryUnitFrame.lua
@@ -201,6 +201,14 @@ function LifeismysteryUnitFrame:Construct(options)
 				colorBinding="NameColor",
 			},
 			{
+				id="labelCombo", type="Label", parent="frame", layer=20,
+				attach = {{ point="BOTTOMRIGHT", element="frame", targetPoint="TOPRIGHT", offsetX=-5, offsetY=17}},
+				visibilityBinding="visibilityCombo",
+				text="{combo}", default="", outline=true, fontSize=25,
+				colorBinding="ComboColor",
+				font = "ArmWrestler",
+			},			
+			{
 				id="labelStatus", type="Label", parent="frame", layer=20,
 				attach = {{ point="CENTERLEFT", element="labelName", targetPoint="CENTERRIGHT", offsetX=0, offsetY=0 }},
 				visibilityBinding="UnitStatus",
@@ -314,6 +322,7 @@ function LifeismysteryUnitFrame:Construct(options)
 		if not options.showAbsorb and element.id == "barAbsorb" then showElement = false end
 		if element.semantic == "HoTPanel" and not options.showHoTPanel then showElement = false	end
 		if options.excludeCasts and ((element.id == "barCast") or (element.id == "labelCast") or (element.id == "labelTime")) then showElement = false end
+		if not options.showCombo and element.id == "labelCombo" then showElement = false end
 		if options.shortname == true and element.id == "labelName" then 
 			element.text = "{nameShort}"
 		elseif	options.shortname == false and element.id == "labelName" then 	
